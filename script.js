b99caa1e9d3c4869da94b06552528642ed26f294
JUAN JOSE <arabas0404@gmail.com>
2026-09-09 21:04:29 -0500

    agregar boton carrito y corregir CSS en JS

--- script.js
+++ script.js
@@ -1,2 +1,10 @@
 const titulo = document.querySelector("h1");
 console.log(titulo);
+titulo.textContent = "¡Bienvenido a mi tienda!";
+const botones = document.querySelectorAll(".btn-agregar");
+
+botones.forEach(function (boton) {
+  boton.addEventListener("click", function () {
+    alert("¡Producto agregado al carrito!");
+  });
+});
